fd253385bdf98e8fce66da330939e338b4a90e24
Adam <ak@psu.edu>
2022-11-09 21:42:53 -0500

    - wrote a test for Ward2017 verifying compatibility with Magpie

---
+++ tests/test_Ward2017.py
@@ -0,0 +1,33 @@
+import unittest
+import csv
+import os
+from pymatgen.core import Structure
+from tqdm import tqdm
+import numpy as np
+from natsort import natsorted
+
+Ward2017 = __import__('descriptorDefinitions.Ward2017', fromlist=[''])
+
+with open('testCaseFiles/exampleInputFilesDescriptorTable.csv', newline='') as f:
+    reader = csv.reader(f)
+    referenceDescriptorTable = list(reader)
+
+labels = referenceDescriptorTable[0]
+testReferenceData = np.float_(referenceDescriptorTable[1:]).tolist()
+skipLabels = ['mean_WCMagnitude_Shell3']
+
+exampleInputFiles = natsorted(os.listdir('testCaseFiles/exampleInputFiles/'))
+testStructures = [Structure.from_file(f'testCaseFiles/exampleInputFiles/{eif}') for eif in exampleInputFiles]
+
+functionOutput = [Ward2017.generate_descriptor(s) for s in tqdm(testStructures[:5])]
+
+class TestWard2017(unittest.TestCase):
+    def test_resutls(self):
+        for fo, trd, name in zip(functionOutput, testReferenceData, exampleInputFiles):
+            for p_fo, p_trd, l in zip(fo, trd, labels):
+                if l not in skipLabels:
+                    with self.subTest(msg=f'Testing {l} calculated for {name}'):
+                        self.assertAlmostEqual(p_fo, p_trd, places=6)
+
+if __name__ == '__main__':
+    unittest.main()
