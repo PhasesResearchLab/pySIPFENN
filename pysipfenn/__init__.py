96132c48d97bb94e7c7c041efed1c3c579612ae3
amkrajewski <ak@psu.edu>
2023-02-28 16:07:57 -0500

    - small feature allowing to open documentation offline if the repository was cloned or the online one if not

--- pysipfenn/__init__.py
+++ pysipfenn/__init__.py
@@ -1,2 +1,13 @@
 from pysipfenn.core.pysipfenn import *
 
+
+def showDocs():
+    """Open the offline documentation in a web browser."""
+    import os
+    if os.path.isfile('docs/_build/index.html'):
+        os.system('open docs/_build/index.html')
+    else:
+        os.system('open https://pysipfenn.org')
+        print('Documentation local files were not found. Please be advised that the documentation is only available if'
+              'you are in cloned pySIPFENN GitHub repository. pySIPFENN will now attempt to visit '
+              'https://pysipfenn.org for the online documentation.')
