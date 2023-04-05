75a01ff92ee871a29fa729986993d05e9292b4ac
amkrajewski <ak@psu.edu>
2023-04-05 13:27:37 -0400

    - improved the structure of Ward2017 test procedure

--- pysipfenn/tests/test_Ward2017.py
+++ pysipfenn/tests/test_Ward2017.py
@@ -8,30 +8,31 @@ from natsort import natsorted
 from importlib import resources
 
 from pysipfenn.descriptorDefinitions import Ward2017
-
-with resources.files('pysipfenn').\
-        joinpath('tests/testCaseFiles/exampleInputFilesDescriptorTable.csv').open('r', newline='') as f:
-    reader = csv.reader(f)
-    referenceDescriptorTable = list(reader)
-
-labels = referenceDescriptorTable[0]
-testReferenceData = np.float_(referenceDescriptorTable[1:]).tolist()
-skipLabels = ['mean_WCMagnitude_Shell3']
-
-with resources.files('pysipfenn').joinpath('tests/testCaseFiles/exampleInputFiles/') as exampleInputsDir:
-    exampleInputFiles = natsorted(os.listdir(exampleInputsDir))
-    testStructures = [Structure.from_file(f'{exampleInputsDir}/{eif}') for eif in exampleInputFiles]
-
-functionOutput = [Ward2017.generate_descriptor(s).tolist() for s in tqdm(testStructures[:5])]
-with resources.files('pysipfenn').\
-        joinpath('tests/Ward2017_TestResult.csv').open('w+', newline='') as f:
-    f.writelines([f'{v}\n' for v in functionOutput[0]])
-
 class TestWard2017(unittest.TestCase):
+
+    def setUp(self):
+        with resources.files('pysipfenn'). \
+                joinpath('tests/testCaseFiles/exampleInputFilesDescriptorTable.csv').open('r', newline='') as f:
+            reader = csv.reader(f)
+            self.referenceDescriptorTable = list(reader)
+
+        self.labels = self.referenceDescriptorTable[0]
+        self.testReferenceData = np.float_(self.referenceDescriptorTable[1:]).tolist()
+        self.skipLabels = ['mean_WCMagnitude_Shell3']
+
+        with resources.files('pysipfenn').joinpath('tests/testCaseFiles/exampleInputFiles/') as exampleInputsDir:
+            self.exampleInputFiles = natsorted(os.listdir(exampleInputsDir))
+            self.testStructures = [Structure.from_file(f'{exampleInputsDir}/{eif}') for eif in self.exampleInputFiles]
+
+        self.functionOutput = [Ward2017.generate_descriptor(s).tolist() for s in tqdm(self.testStructures[:5])]
+        with resources.files('pysipfenn'). \
+                joinpath('tests/Ward2017_TestResult.csv').open('w+', newline='') as f:
+            f.writelines([f'{v}\n' for v in self.functionOutput[0]])
+
     def test_resutls(self):
-        for fo, trd, name in zip(functionOutput, testReferenceData, exampleInputFiles):
-            for p_fo, p_trd, l in zip(fo, trd, labels):
-                if l not in skipLabels:
+        for fo, trd, name in zip(self.functionOutput, self.testReferenceData, self.exampleInputFiles):
+            for p_fo, p_trd, l in zip(fo, trd, self.labels):
+                if l not in self.skipLabels:
                     with self.subTest(msg=f'Testing {l} calculated for {name}'):
                         self.assertAlmostEqual(p_fo, p_trd, places=6)
 
