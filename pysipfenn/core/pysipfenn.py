55d14ce4ee7cfd63c7e6c087e95ab5955fa68d36
Adam Krajewski <54290107+amkrajewski@users.noreply.github.com>
2023-06-25 09:47:35 -0700

    - typo fix

--- pysipfenn/core/pysipfenn.py
+++ pysipfenn/core/pysipfenn.py
@@ -354,7 +354,7 @@ class Calculator:
             t0 = perf_counter()
             model = models[net]
             model.eval()
-            if 'OnnxDropoutDynamic()' in {str(module) for module in list(m._modules.values())}:
+            if 'OnnxDropoutDynamic()' in {str(module) for module in list(model._modules.values())}:
                 tempOut = model(dataIn, None)
             else:
                 tempOut = model(dataIn)
