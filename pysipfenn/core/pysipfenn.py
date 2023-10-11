1b310f2403abbdc0e70356a4613772a9dc9a5698
amkrajewski <ak@psu.edu>
2023-10-11 12:25:15 -0400

    - adjusted to the correct version in pysipfenn module

--- pysipfenn/core/pysipfenn.py
+++ pysipfenn/core/pysipfenn.py
@@ -24,7 +24,7 @@ from pysipfenn.descriptorDefinitions import Ward2017, KS2022, KS2022_dilute
 
 # - add new ones here if extending the code
 
-__version__ = '0.11.0post1'
+__version__ = '0.13.0'
 __authors__ = [["Adam Krajewski", "ak@psu.edu"],
                ["Jonathan Siegel", "jwsiegel@tamu.edu"]]
 __name__ = 'pysipfenn'
