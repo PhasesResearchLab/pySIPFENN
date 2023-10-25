f307f6b4201785dcffae2d3f2d32e6acadc45a3b
amkrajewski <ak@psu.edu>
2023-10-25 13:38:29 -0400

    (randSol) improved RS test documentation

--- pysipfenn/tests/test_KS2022_randomSolution.py
+++ pysipfenn/tests/test_KS2022_randomSolution.py
@@ -11,8 +11,8 @@ class TestKS2022(unittest.TestCase):
     def setUp(self):
         '''Load the feature ranges and means from the test file as calculated for the testing/profiling case included
         in the KS2022_randomSolution script run for 50 iterations over BCC Cr12.8 Fe12.8 Co12.8 Ni12.8 Cu12.8 Al35.9
-        alloy.'''
-
+        alloy.
+        '''
         testFilePath = 'tests/testCaseFiles/TestFile_DescriptorData_KS2022_randomSolution_valueRangesMeans.csv'
         with resources.files('pysipfenn').joinpath(testFilePath).open('r', newline='') as f:
             reader = csv.reader(f)
@@ -25,12 +25,11 @@ class TestKS2022(unittest.TestCase):
     def test_results(self):
         '''Test the descriptor generation function by comparing the results feature by feature to the reference data
         statistics, testing whether the result is within the observed range of values from the observed mean value
-        allowing for additional 1% deviation from the mean value to handle numerical precision in cases where the
-        feature converges to exactly the mean value with near-zero range (e.g. coordination number in BCC in case of
-        ideal lattice positions).'''
-
+        allowing for additional 2% deviation from the mean value to handle numerical precision in cases where the
+        feature converges to near exactly the mean value with near-zero range (e.g. coordination number in BCC in case
+        of ideal lattice positions).
+        '''
         testValues = KS2022_randomSolutions.profile(test='BCC', returnDescriptor=True)
-
         for testValue, descriptorRange, descriptorMean, label in zip(
                 testValues,
                 self.descriptorRangeList,
@@ -41,10 +40,11 @@ class TestKS2022(unittest.TestCase):
                 self.assertLessEqual(testValue, (1.02*descriptorMean)+descriptorRange)
 
 
-class TestKS2022Profiling(unittest.TestCase):
-    '''Test the KS2022 descriptor generation by profiling the execution time of the descriptor generation function
-        for two example structures in serial and parallel (8 workers) mode.'''
-
+class TestKS2022RandomSolutionProfiling(unittest.TestCase):
+    '''Test the KS2022 RS descriptor generation by profiling the execution time for example composition for each
+    common alloy structure: BCC, FCC, HCP. The profiling is serial inside each task, but done in parallel across
+    structures.
+    '''
     def test_serialInParallel(self):
         process_map(KS2022_randomSolutions.profile,
                     ['BCC', 'FCC', 'HCP'],
