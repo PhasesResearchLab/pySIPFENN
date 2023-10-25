465f9b5c28651f9b865f922cf665150cea121118
amkrajewski <ak@psu.edu>
2023-10-25 11:15:19 -0400

    - (docs) improved/filled documentation for Ward2017 correctness testing

--- pysipfenn/tests/test_Ward2017.py
+++ pysipfenn/tests/test_Ward2017.py
@@ -8,9 +8,18 @@ from natsort import natsorted
 from importlib import resources
 
 from pysipfenn.descriptorDefinitions import Ward2017
-class TestWard2017(unittest.TestCase):
 
+
+class TestWard2017(unittest.TestCase):
+    '''Tests the correctness of the KS2022 descriptor generation function by comparing the results to the reference data
+    for the first 5 structures in the exampleInputFiles directory, stored in the exampleInputFilesDescriptorTable.csv.
+    '''
     def setUp(self):
+        '''Reads the reference data from the exampleInputFilesDescriptorTable.csv file and the labels from the first
+        row of that file. Then it reads the first 5 structures from the exampleInputFiles directory and generates the
+        descriptors for them. The results are stored in the functionOutput list. It also persists the test results in
+        the Ward2017_TestResult.csv file.
+        '''
         with resources.files('pysipfenn'). \
                 joinpath('tests/testCaseFiles/exampleInputFilesDescriptorTable.csv').open('r', newline='') as f:
             reader = csv.reader(f)
@@ -30,6 +39,8 @@ class TestWard2017(unittest.TestCase):
             f.writelines([f'{v}\n' for v in self.functionOutput[0]])
 
     def test_resutls(self):
+        '''Compares the results of the Ward2017 descriptor generation function to the reference data on a field-by-field
+        basis by requiring the absolute difference to be less than 1e-6.'''
         for fo, trd, name in zip(self.functionOutput, self.testReferenceData, self.exampleInputFiles):
             for p_fo, p_trd, l in zip(fo, trd, self.labels):
                 if l not in self.skipLabels:
@@ -39,12 +50,17 @@ class TestWard2017(unittest.TestCase):
 
 class TestWard2017Profiling(unittest.TestCase):
     '''Test the Ward2017 descriptor generation by profiling the execution time of the descriptor generation function
-    for two example structures in serial and parallel (8 workers) mode.'''
+    for two example structures (JVASP-10001 and diluteNiAlloy).
+    '''
     def test_serial(self):
+        '''Test the serial execution of the descriptor generation function 4 times for each of the two examples.'''
         Ward2017.profile(test='JVASP-10001', nRuns=4)
         Ward2017.profile(test='diluteNiAlloy', nRuns=4)
 
     def test_parallel(self):
+        '''Test the parallel execution of the descriptor generation function 24 times for each of the two examples
+        but in parallel with up to 8 workers to speed up the execution.
+        '''
         Ward2017.profileParallel(test='JVASP-10001', nRuns=24)
         Ward2017.profileParallel(test='diluteNiAlloy', nRuns=24)
 
