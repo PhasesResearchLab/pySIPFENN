1db2bec3ce3ba7028a3bf246475fcaaa40c19744
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2024-02-20 23:39:04 -0500

    (core) bugfix for a typo causing `verbose=False` to cause an error in KS2022 `runModels` function

--- pysipfenn/core/pysipfenn.py
+++ pysipfenn/core/pysipfenn.py
@@ -72,7 +72,7 @@ class Calculator:
         """Initializes the pySIPFENN Calculator object."""
         if verbose:
             print('*********  Initializing pySIPFENN Calculator  **********')
-            self.verbose = verbose
+        self.verbose = verbose
         # dictionary with all model information
         with resources.files('pysipfenn.modelsSIPFENN').joinpath('models.json').open('r') as f:
             if verbose:
