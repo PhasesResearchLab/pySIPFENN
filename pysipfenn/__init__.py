b7bb2c43e30a33e0575e8a629e906c608e74ef28
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2024-03-29 14:04:52 -0400

    (core) reverted the `__init__.py` scope narrowing for the top level import

--- pysipfenn/__init__.py
+++ pysipfenn/__init__.py
@@ -1,4 +1,4 @@
 # Importing from top pySIPFENN namespace...
-from pysipfenn.core.pysipfenn import Calculator, ward2ks2022, overwritePrototypeLibrary, string2prototype
-from pysipfenn.core.modelExporters import ONNXExporter, TorchExporter, CoreMLExporter
-from pysipfenn.core.modelAdjusters import LocalAdjuster, OPTIMADEAdjuster
+from pysipfenn.core.pysipfenn import *
+from pysipfenn.core.modelExporters import *
+from pysipfenn.core.modelAdjusters import *
