2cbcbc150ec771415264fe8f07959eed090c44d9
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2024-03-29 13:04:39 -0400

    (core) limit the convenience import scope of the top level `__init__.py`

--- pysipfenn/__init__.py
+++ pysipfenn/__init__.py
@@ -1,4 +1,4 @@
 # Importing from top pySIPFENN namespace...
-from pysipfenn.core.pysipfenn import *
-from pysipfenn.core.modelExporters import *
-from pysipfenn.core.modelAdjusters import *
+from pysipfenn.core.pysipfenn import Calculator, ward2ks2022
+from pysipfenn.core.modelExporters import ONNXExporter, TorchExporter, CoreMLExporter
+from pysipfenn.core.modelAdjusters import LocalAdjuster, OPTIMADEAdjuster
